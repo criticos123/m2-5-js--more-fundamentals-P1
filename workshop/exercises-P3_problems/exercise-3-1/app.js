0ba22fcb5c4cde163c791f5cf0ba17d370ea2b52
Stephanos Criticos <scritico@unb.ca>
2020-06-22 21:21:23 -0400

    exercise3-1

--- workshop/exercises-P3_problems/exercise-3-1/app.js
+++ workshop/exercises-P3_problems/exercise-3-1/app.js
@@ -6,3 +6,27 @@
 // Guidelines
 // - write only one event listener
 // - use a switch statement
+const body = document.querySelector("body");
+element = document.querySelector(".buttons");
+function clicker(event) {
+  const buttontarget = event.target.id;
+  const buttonid = document.getElementById(buttontarget);
+  switch (buttontarget) {
+    case "btn1":
+      buttonid.style.opacity = 0;
+      buttonid.classList.toggle("buttons");
+      break;
+    case "btn2":
+      buttonid.style.backgroundColor = "crimson";
+      break;
+    case "btn3":
+      buttonid.style.backgroundColor = "lightblue";
+      break;
+    case "btn4":
+      buttonid.classList.add("jitters");
+      break;
+    default:
+      break;
+  }
+}
+element.addEventListener("click", clicker);
